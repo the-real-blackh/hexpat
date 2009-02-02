167340a90c9e9f4e88d13ed5be5a7166fee63f4a
oversensitive.pastors.stephen <oversensitive.pastors.stephen@blacksapphire.com>
2009-02-02 22:22:15 +1300

    Improve test2.hs
    darcs-hash:20090202092215-ed7c9-966c0637b5ff31fda3e52ff7179786d07e088f88.gz

--- test2.hs
+++ test2.hs
@@ -11,13 +11,19 @@ toByteString = B.pack . map (fromIntegral . ord)
 test :: String -> IO ()
 test xml = do
   let mTree = T.parse (Just UTF8) (toByteString xml)
-  putStrLn $ show mTree
+  --putStrLn $ show mTree
   case mTree of
-      Just tree ->
-          putStrLn $ formatDoc (Just UTF8) tree
+      Just tree -> do
+          let out = formatDoc (Just UTF8) tree
+          if out == xml
+              then putStrLn "passed"
+              else do
+                  putStrLn $ "FAILED: mismatch"
+                  putStrLn $ "input="++xml
+                  putStrLn $ "output="++out
       Nothing ->
-          putStrLn "parse failed!"
+          putStrLn "FAILED: parse failed"
 
 main = do
-  test "<?xml version=\"1.0\"?>\n<test><test1 type=\"expression\">Cat &amp; mouse</test1><test2 type=\"communication\">Dog &amp; bone</test2></test>"
-  test "<?xml version=\"1.0\"?>\n<test>Cat &amp; mouse</test>"
+  test "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<test><test1 type=\"expression\">Cat &amp; mouse</test1><test2 type=\"communication\">Dog &amp; bone</test2></test>"
+  test "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<test>Cat &amp; mouse</test>"
