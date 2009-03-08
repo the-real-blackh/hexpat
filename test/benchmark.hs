93bb5371a35c97bc5a5513cc453e49bb0b392d91
oversensitive.pastors.stephen <oversensitive.pastors.stephen@blacksapphire.com>
2009-03-09 11:28:22 +1300

    Add some benchmarking results to the haddock docs
    Ignore-this: 101e25e61b579b3ecef2e5c6a71fbd28
    
    darcs-hash:20090308222822-ed7c9-7ce8ea3942945bec2ab2c95c3ec2c31d9bf5de4f.gz

--- test/benchmark.hs
+++ test/benchmark.hs
@@ -17,6 +17,7 @@ import System.IO
 import Foreign.ForeignPtr
 import Foreign.Ptr
 import Microbench
+import Text.XML.HaXml as HaXml
 
 instance NFData B.ByteString where
     rnf bs = ()
@@ -24,41 +25,53 @@ instance NFData B.ByteString where
 instance NFData T.Text where
     rnf bs = ()
 
-parseOnly :: L.ByteString -> IO ()
-parseOnly doc = do
+instance NFData Document where
+    rnf (Document _ _ root _) = rnf root
+
+instance NFData Element where
+    rnf (Elem name attrs content) = rnf (name, attrs, content)
+
+instance NFData AttValue where
+    rnf (AttValue val) = rnf val
+
+instance NFData Reference where
+    rnf ref = ()
+
+instance NFData Content where
+    rnf content = ()
+
+parseOnly :: L.ByteString -> String -> IO ()
+parseOnly xml _ = do
     parser <- newParser Nothing
-    parse parser doc
+    parse parser xml
     return ()
 
-tests :: [(String, L.ByteString -> IO ())]
+tests :: [(String, L.ByteString -> String -> IO ())]
 tests = [
+    ("HaXml", \_ xml -> rnf (HaXml.xmlParse "input" xml) `seq` return ()),
     ("low-level parse only, no tree", parseOnly),
-    ("lazy parseTree stringFlavor", \doc -> rnf (parseTree stringFlavor Nothing doc) `seq` return ()),
-    ("lazy parseTree byteStringFlavor", \doc -> rnf (parseTree byteStringFlavor Nothing doc) `seq` return ()),
-    ("lazy parseTree textFlavor", \doc -> rnf (parseTree textFlavor Nothing doc) `seq` return ()),
-    ("lazy parseTree qualifiedStringFlavor", \doc -> rnf (parseTree qualifiedStringFlavor Nothing doc) `seq` return ()),
-    ("lazy parseTree qualifiedByteStringFlavor", \doc -> rnf (parseTree qualifiedByteStringFlavor Nothing doc) `seq` return ()),
-    ("lazy parseTree qualifiedTextFlavor", \doc -> rnf (parseTree qualifiedTextFlavor Nothing doc) `seq` return ()),
-    ("strict parseTree' stringFlavor", \doc -> rnf (parseTree' stringFlavor Nothing doc) `seq` return ()),
-    ("strict parseTree' byteStringFlavor", \doc -> rnf (parseTree' byteStringFlavor Nothing doc) `seq` return ()),
-    ("strict parseTree' textFlavor", \doc -> rnf (parseTree' textFlavor Nothing doc) `seq` return ()),
-    ("strict parseTree' qualifiedStringFlavor", \doc -> rnf (parseTree' qualifiedStringFlavor Nothing doc) `seq` return ()),
-    ("strict parseTree' qualifiedByteStringFlavor", \doc -> rnf (parseTree' qualifiedByteStringFlavor Nothing doc) `seq` return ()),
-    ("strict parseTree' qualifiedTextFlavor", \doc -> rnf (parseTree' qualifiedTextFlavor Nothing doc) `seq` return ())
+    ("lazy parseTree stringFlavor", \xml _ -> rnf (parseTree stringFlavor Nothing xml) `seq` return ()),
+    ("lazy parseTree byteStringFlavor", \xml _ -> rnf (parseTree byteStringFlavor Nothing xml) `seq` return ()),
+    ("lazy parseTree textFlavor", \xml _ -> rnf (parseTree textFlavor Nothing xml) `seq` return ()),
+    ("lazy parseTree qualifiedStringFlavor", \xml _ -> rnf (parseTree qualifiedStringFlavor Nothing xml) `seq` return ()),
+    ("lazy parseTree qualifiedByteStringFlavor", \xml _ -> rnf (parseTree qualifiedByteStringFlavor Nothing xml) `seq` return ()),
+    ("lazy parseTree qualifiedTextFlavor", \xml _ -> rnf (parseTree qualifiedTextFlavor Nothing xml) `seq` return ()),
+    ("strict parseTree' stringFlavor", \xml _ -> rnf (parseTree' stringFlavor Nothing xml) `seq` return ()),
+    ("strict parseTree' byteStringFlavor", \xml _ -> rnf (parseTree' byteStringFlavor Nothing xml) `seq` return ()),
+    ("strict parseTree' textFlavor", \xml _ -> rnf (parseTree' textFlavor Nothing xml) `seq` return ()),
+    ("strict parseTree' qualifiedStringFlavor", \xml _ -> rnf (parseTree' qualifiedStringFlavor Nothing xml) `seq` return ()),
+    ("strict parseTree' qualifiedByteStringFlavor", \xml _ -> rnf (parseTree' qualifiedByteStringFlavor Nothing xml) `seq` return ()),
+    ("strict parseTree' qualifiedTextFlavor", \xml _ -> rnf (parseTree' qualifiedTextFlavor Nothing xml) `seq` return ())
   ]
 
 myCopy :: B.ByteString -> IO B.ByteString
 myCopy (I.PS x s l) = I.create l $ \p -> withForeignPtr x $ \f ->
     I.memcpy p (f `plusPtr` s) (fromIntegral l)
 
-myLCopy :: L.ByteString -> IO L.ByteString
-myLCopy bs = do
-    let cs = L.toChunks bs
-    cs' <- mapM myCopy cs
-    return $ L.fromChunks cs'
-
 main = do
-    doc0 <- B.readFile "test.xml"
-    let doc = L.fromChunks [doc0]
-    forM_ tests $ \(a,b) -> microbench a $ myLCopy doc >>= b
+    xml <- B.readFile "test.xml"
+    forM_ tests $ \(a,b) -> microbench a $ do
+        copy <- myCopy xml
+        let xmlStr = map w2c $ B.unpack copy
+        b (L.fromChunks [copy]) xmlStr
 
